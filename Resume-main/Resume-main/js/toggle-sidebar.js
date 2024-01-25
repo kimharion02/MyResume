efd489deae29b1a7060647ba5dd8e5042a118d49
kimharion02 <157724708+kimharion02@users.noreply.github.com>
2024-01-26 01:59:08 +0800

    Add files via upload

---
+++ Resume-main/Resume-main/js/toggle-sidebar.js
@@ -0,0 +1,4 @@
+function toggleSidebar() {
+    var sidebar = document.querySelector('.menu'); // Update this selector to match your menu class
+    sidebar.style.width = (sidebar.style.width === '300px') ? '0' : '300px'; // Adjust the width as needed
+}
