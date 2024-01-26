e2a622d4ee9aaaaefb8498d1b88be0669dc74317
kimharion02 <157724708+kimharion02@users.noreply.github.com>
2024-01-26 12:26:07 +0800

    Delete Resume-main directory

--- Resume-main/Resume-main/js/toggle-sidebar.js
+++
@@ -1,4 +0,0 @@
-function toggleSidebar() {
-    var sidebar = document.querySelector('.menu'); // Update this selector to match your menu class
-    sidebar.style.width = (sidebar.style.width === '300px') ? '0' : '300px'; // Adjust the width as needed
-}
